619dcfaa1f3bf9e847da09cec8c33336dd2f831a
helius <Shiwenhao_0218@163.com>
2019-10-20 02:45:52 +0800

    登录页完成设置全局守卫导航拦截跳转到首页

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,35 +1,76 @@
 <template>
   <div class="login">
 <!-- 表单组件 -->
-    <el-form ref="form" :model="form" label-width="80px">
+    <el-form ref="form" :rules="rules" :model="form" label-width="80px">
       <img src="../assets/avatar.jpg" alt="">
-      <el-form-item label="用户名">
+      <el-form-item label="用户名" prop="username">
         <el-input placeholder="请输入用户名" v-model="form.username"></el-input>
       </el-form-item>
-      <el-form-item label="密码">
+      <el-form-item label="密码" prop="password">
         <el-input placeholder="请输入密码" type="password" v-model="form.password"></el-input>
       </el-form-item>
       <el-form-item>
-        <el-button class="loginBtn" type="primary">登录</el-button>
-        <el-button>重置</el-button>
+        <el-button @click="login" class="loginBtn" type="primary">登录</el-button>
+        <el-button @click="reset">重置</el-button>
       </el-form-item>
     </el-form>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
     return {
       form: {
         username: '',
         password: ''
+      },
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: ['blur', 'change'] },
+          { min: 3, max: 5, message: '长度在3-5个字符', trigger: ['blur', 'change'] }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: ['blur', 'change'] },
+          { min: 3, max: 12, message: '密码长度在3-12个字符', trigger: ['blur', 'change'] }
+        ]
       }
     }
   },
   methods: {
-    onSubmit () {
-      console.log('submit!')
+    reset () {
+      this.$refs.form.resetFields()
+    },
+    login () {
+      this.$refs.form.validate(isValid => {
+        if (!isValid) return
+        // axios({
+        //   url: 'http://localhost:8888/api/private/v1/login',
+        //   method: 'post',
+        //   data: this.form
+        // }).then()
+        axios.post('http://localhost:8888/api/private/v1/login', this.form).then(res => {
+          const { meta, data } = res.data
+          if (meta.status === 200) {
+            // console.log(meta.msg)
+            localStorage.setItem('token', data.token)
+            this.$message({
+              type: 'success',
+              message: meta.msg,
+              duration: 1000
+            })
+            this.$router.push('/Index')
+          } else {
+            // console.log(meta.msg)
+            this.$message({
+              type: 'error',
+              message: meta.msg,
+              duration: 1000
+            })
+          }
+        })
+      })
     }
   }
 }
--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
+import Index from '../components/Index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/Index'
+  },
+  {
+    path: '/Login',
     component: Login
+  },
+  {
+    path: '/Index',
+    component: Index
   }
 ]
 
@@ -15,4 +24,12 @@ const router = new VueRouter({
   routes
 })
 
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token')
+  if (token || to.path === '/login') {
+    next()
+  } else {
+    next('/login')
+  }
+})
 export default router
