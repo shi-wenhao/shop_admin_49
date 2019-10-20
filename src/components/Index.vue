1c34924148282020786de1b8b254fa79deea31fe
helius <Shiwenhao_0218@163.com>
2019-10-20 11:52:22 +0800

    首页静态页面完成

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,13 +1,116 @@
   <template>
-  <div class="index">首页</div>
+    <el-container class="index">
+      <el-header>
+        <div class="logo">
+          <img src="../assets/logo.ec55f206.png" alt="">
+        </div>
+        <div class="title">
+          <h1>Eコマースのバックグラウンド管理</h1>
+        </div>
+        <div class="logout">
+          ようこそ~
+          <a @click="logout" href="javascript:;">ログアウト</a>
+        </div>
+      </el-header>
+      <el-container>
+        <el-aside width="200px">
+          <el-menu
+            unique-opened
+            class="el-menu-vertical-demo"
+            background-color="#545c64"
+            text-color="#fff"
+            active-text-color="#ffd04b">
+            <el-submenu index="1">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>ユーザー管理</span>
+              </template>
+              <el-menu-item index="1-1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">ユーザーリスト</span>
+              </el-menu-item>
+            </el-submenu>
+
+            <el-submenu index="2">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>権利管理</span>
+              </template>
+              <el-menu-item index="2-1">
+                <i class="el-icon-menu"></i>
+                <span slot="title">役割リスト</span>
+              </el-menu-item>
+              <el-menu-item index="2-2">
+                <i class="el-icon-menu"></i>
+                <span slot="title">許可リスト</span>
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout () {
+      this.$confirm('亲，你确定要退出系统吗？', '温馨提示', {
+        // confirmButtonText: '确定',
+        // cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        localStorage.removeItem('token')
+        this.$router.push('/login')
+        this.$message.success('退出成功')
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="scss" scoped>
+.index {
+  height: 100%;
+  .el-header {
+    background-color: #d8d8d8;
+    display: flex;
+    .logo {
+      width: 180px;
+      img {
+        height: 40px;
+        margin:10px;
+      }
+    }
+    .title {
+      flex:1;
+      color: #545c64;
+      line-height: 60px;
+      text-align: center;
+      h1 {
+        height: 60px;
+      }
+    }
+    .logout {
+      width: 180px;
+      text-align: right;
+      line-height: 60px;
+      a {
+        color: orange;
+      }
+    }
+  }
+  .el-aside{
+    background-color: #545c64;
+    .el-menu {
+      border-right: none;
+    }
+  }
+  .el-main {
+    background: #ecf0f1;
+  }
+}
 </style>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -76,7 +76,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
   .login {
     width: 100%;
     height: 100%;
